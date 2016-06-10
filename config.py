48268dfed4e39c3e7291a38288a78813ae58d463
nevermoreluo <656763371@qq.com>
2016-06-10 11:34:01 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-06-08 11:07:06
+# @Author  : Nevermoreluo (nevermoreluo@gmail.com)
+
+
+imgfile = 'screenshot%s.jpg'
+
+# 设置phantomjs路径
+phantomjs_path = ''
+userSettingDate = '.userSetting'
