0cd882bcd10b75d61063dbf9fbee895d8124020e
nevermoreluo <656763371@qq.com>
2016-06-23 12:34:00 +0800

    Update mainWindow.py

--- mainWindow.py
+++ mainWindow.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # @Date    : 2016-06-08 11:47:55
 # @Author  : Nevermoreluo (nevermoreluo@gmail.com)
 import sys
@@ -9,7 +8,10 @@ from sysOption import SysOption
 
 
 class MainDialog(QtGui.QWidget):
-
+    '''
+    主窗口界面，包括两个Tab页
+    下单界面以及系统设置
+    '''
     def __init__(self, parent=None):
         super(MainDialog, self).__init__(parent)
         tabWidget = QtGui.QTabWidget(self)
@@ -17,12 +19,18 @@ class MainDialog(QtGui.QWidget):
         self.automation_tab = Automation()
         tabWidget.addTab(self.automation_tab, self.tr("自动化下单"))
         tabWidget.addTab(system_tab, self.tr('系统设置'))
+        # 网格布局
         self.mainLayout = QtGui.QGridLayout(self)
         self.mainLayout.addWidget(tabWidget)
+        # 设置标题
         self.setWindowTitle(u'吾宁当当自动下单')
+        # 设置500左右避免某些低分辨率电脑界面超出屏幕范围
         self.resize(1000, 500)
 
     def center(self):
+        '''
+        设置主界面在初始化的时候在屏幕中央
+        '''
         # 获得窗口的数据,PySide.QtCore.QRect
         qr = self.frameGeometry()
         # 获得本机可视窗口的中心点坐标
@@ -33,6 +41,9 @@ class MainDialog(QtGui.QWidget):
         self.move(qr.topLeft())
 
     def closeEvent(self, event):
+        '''
+        设置窗口关闭事件
+        '''
         # 将弹出窗口的结果赋值给reply
         reply = QtGui.QMessageBox.question(self, 'Message',
                                            u"确定要关闭当前窗口吗?",
@@ -41,6 +52,7 @@ class MainDialog(QtGui.QWidget):
                                            QtGui.QMessageBox.No)
         # 检验用户返回，并返回对应的结果
         if reply == QtGui.QMessageBox.Yes:
+            # 关闭窗口停止正在运行的browser
             if hasattr(self.automation_tab, 'threadsList'):
                 [t.dangDangXiaDan.browser.close()
                  for t in self.automation_tab.threadsList
